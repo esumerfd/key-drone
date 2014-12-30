1199b4fa90ab5fcb594b967eaeb37ab92c2acfb5
Ed Sumerfield <esumerfield@ipcoop.com>
2014-12-30 12:20:00 -0500

    clean lein project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to key-drone
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/key_drone/core_test.clj
@@ -0,0 +1,7 @@
+(ns key-drone.core-test
+  (:require [clojure.test :refer :all]
+            [key-drone.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

17622cdf5384d5b8e37c6377c8fc31223c9952c4
Ed Sumerfield <esumerfield@ipcoop.com>
2014-12-31 17:04:00 -0500

    add quit key recognition and some tests

--- src/key_drone/core.clj
+++ src/key_drone/core.clj
@@ -4,24 +4,59 @@
   (:import  [jline.console ConsoleReader])
   (:gen-class :main true))
 
-(defn key-drone
-  "Key drone startup"
-  []
-  (println "Starting Key Drone")
-
-  (while true
-    (let [cr (ConsoleReader.)
-          keyint (.readCharacter cr)]
-      (println (format "Got %d ('%c')!" keyint (char keyint)))))
-
-  ;(drone-initialize)
+;input: ascii code of key
+;transform: apply function associsated with key
+;output: true/false for quit key pressed
+;implements interfaces:
+; - control handler (output true/false)
+; - key press handler (input asciiCode, output: true/false)
+(defn key-handler
+  [asciiCode]
 
   ;;Some stuff that will move
   ;(drone :take-off)
-  ;(println "Waiting for drone to takeoff")
   ;(Thread/sleep 5000)
   ;(drone :land)
 
+   (not= "Q" (clojure.string/upper-case (char asciiCode)))
+  )
+
+;input: key press handler
+;transform: input key strokes
+;output; true/false from handler
+;requires interface:
+; - key press handler
+(defn pressed-keys
+  "generates keystrokes"
+  [key-press-handler]
+
+  (let [reader (ConsoleReader.) 
+        asciiCode (.readCharacter reader)]
+    (key-press-handler asciiCode)
+    )
+  )
+
+;input: control handler
+;transform: call handler until false
+;output: false
+;requires interface:
+; - control handler
+(defn control-loop
+  [control-handler]
+  (while (pressed-keys control-handler))
+  )
+
+;input: none
+;transform: started application into drone control
+;output: exit code 0 for success else failure
+(defn key-drone
+  "Key drone startup"
+  []
+  (println "Starting Key Drone (q to quit)")
+
+  ;(drone-initialize)
+
+  (control-loop key-handler)
 
   (int 0))
 
@@ -29,3 +64,4 @@
   "Command  line entry point for key-drone"
   [& args]
   (key-drone))
+
--- test/key_drone/core_test.clj
+++ test/key_drone/core_test.clj
@@ -2,7 +2,23 @@
   (:require [clojure.test :refer :all]
             [key-drone.core :refer :all]))
 
-(deftest runs-main
-  (testing "runs main"
-    (is (= 0 (key-drone)))))
+(deftest pressed-keys-test
+  ;(testing "pressed-keys"
+  ;(is (= 13 (pressed-keys #(+ 0 %)))))
+  )
+
+(deftest key-handler-test
+
+  (testing "navigation character"
+    (is (= true (key-handler 101)))
+    )   
+
+  (testing "lower case quit character"
+    (is (= false (key-handler 113)))
+    )
+
+  (testing "upper case quit character"
+    (is (= false (key-handler 81)))
+    )
+  )
 

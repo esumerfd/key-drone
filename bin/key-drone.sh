27fb3cc5c3132ae6577a19c644e95403d5d59b5e
Ed Sumerfield <esumerfield@ipcoop.com>
2014-12-31 17:31:19 -0500

    add bash script launcher

---
+++ bin/key-drone.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# #################################################################################
+# key-drone
+#
+# Keyboard control of an AR Drone.
+# #################################################################################
+
+if [[ -z $CLOJURE_HOME ]]; then
+  echo "Must set CLOJURE_HOME environment variable"
+  exit 1
+fi
+
+KEY_DRONE_HOME=$(cd `dirname $0`/..; echo $PWD)
+
+java -cp $CLOJURE_HOME/target/clojure-1.6.0.jar -jar $KEY_DRONE_HOME/target/key-drone*standalone.jar 
+
